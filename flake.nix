9a620317f5e809702acb64dc8f03fc367ca77b9e
Vincent Nys <vincentnys@gmail.com>
2025-07-11 11:49:43 +0200

    ACreate a user data directory outside the store.

--- flake.nix
+++ flake.nix
@@ -7,34 +7,49 @@
     { self, nixpkgs }:
     let
       pkgs = nixpkgs.legacyPackages.x86_64-linux;
-      remarkableApp = pkgs.writeShellApplication {
-        name = "remarkable";
-        runtimeInputs = [ pkgs.wineWowPackages.waylandFull ];
-        text = ''
-          export WINEPREFIX=`${self}/wineprefix`
-          export WINEARCH=win64
-          export WINEDLLOVERRIDES="qnetworklistmanager=b"
-          exec wine `${self}/remarkable/reMarkable.exe`
-        '';
-      };
+
     in
     {
 
-      packages.x86_64-linux.default = remarkableApp;
+      packages.x86_64-linux.default = pkgs.writeShellApplication {
+        name = "remarkable";
+        runtimeInputs = [ pkgs.wineWowPackages.waylandFull ];
+        text = ''
+          PREFIX_TEMPLATE="${self}/wineprefix"
+          WINEPREFIX="''${XDG_DATA_HOME:-$HOME/.local/share}/remarkable-wineprefix"
+
+            if [ ! -d "$WINEPREFIX" ]; then
+              echo "First launch: copying Wine prefix to $WINEPREFIX..."
+              mkdir -p "$(dirname "$WINEPREFIX")"
+              cp -r "$PREFIX_TEMPLATE" "$WINEPREFIX"
+            fi
+          export WINEPREFIX
+          export WINEARCH=win64
+          export WINEDLLOVERRIDES="qnetworklistmanager=b"
+          exec wine ${self}/remarkable/reMarkable.exe
+        '';
+      };
 
       devShells.x86_64-linux.default = pkgs.mkShell {
         packages = with nixpkgs.legacyPackages.x86_64-linux; [
           wineWowPackages.waylandFull
           winetricks
           bash
-          remarkableApp
         ];
 
         shellHook = ''
-          export WINEPREFIX=${self}/wineprefix
+          PREFIX_TEMPLATE="${self}/wineprefix"
+          WINEPREFIX="''${XDG_DATA_HOME:-$HOME/.local/share}/remarkable-wineprefix"
+
+            if [ ! -d "$WINEPREFIX" ]; then
+              echo "First launch: copying Wine prefix to $WINEPREFIX..."
+              mkdir -p "$(dirname "$WINEPREFIX")"
+              cp -r "$PREFIX_TEMPLATE" "$WINEPREFIX"
+            fi
+          export WINEPREFIX
           export WINEARCH=win64
           export WINEDLLOVERRIDES="qnetworklistmanager=b"
-          echo "To run, run: remarkable"
+          echo "To run, run: wine remarkable/reMarkable.exe"
         '';
       };
     };
